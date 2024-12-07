0fb33eac12e26a06e7f7b1e2ab6d7ee465ddf9aa
mahdiassy <assimahdi1144@gmail.com>
2024-12-07 21:30:01 +0200

    zbttconnection

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const sequelize = require('./models/db');
+const sequelize = require('./config/db');
 
 
 dotenv.config();
